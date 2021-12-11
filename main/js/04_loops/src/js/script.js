e7ad35b0653b903ef51d874eceeb0815450ab78a
Viktor Dehtiarov <4059858@gmail.com>
2021-12-11 15:14:21 +0200

    Lesson 29. Js - 4. Loops

---
+++ main/js/04_loops/src/js/script.js
@@ -0,0 +1,96 @@
+// const num = +prompt();
+// let i = num;
+// let r = '';
+// console.log(num, i);
+// while (num > i++) {
+//     r = r + '#';
+
+//     if (i !== num) {
+//         r = r + '-';
+//     }
+
+//     console.log('i:' + i, 'num:' + num, r);
+//}
+// console.log(r);
+// 543210
+// 43210
+// 3210
+// 210
+// 10
+// 0
+// "#-#-#"
+
+let z = 50;
+
+// while (z > 50) {
+//     if (!(z-- % 2) && z !== 0) continue;
+//     console.log(z);
+
+//     if (z < 0) {
+//         break;
+//     }
+// }
+// 2 + 2 * 2
+// let d = '';
+// do {
+//     d = 'ee';
+//     if (z-- < 0) {
+//         break;
+//     }
+//     // if (r !== 6) l = confirm();
+// } while (d && l);
+
+// //console.log();
+
+// const ttt = "sdfsdfsdfsdfsdfsdfsdf"; // => "sDfSdFsDfSdFsDfSdFsDf"
+// let eee = '';
+
+
+
+// for (let i = 0; i < ttt.length; i++) {
+
+//     if (i % 2 !== 0) {
+//         eee = eee + ttt[i].toLocaleUpperCase();
+//         continue;
+//     }
+
+//     eee = eee + ttt[i];
+
+//     console.log(ttt[i].toLocaleUpperCase(), i);
+
+//     if (z-- < 0) break;
+// }
+
+// console.log(eee);
+
+
+const ggg = +prompt();
+let res = '';
+qwer: for (let i = 1; i <= ggg; i++) {
+
+    for (let y = 1; y <= ggg; y++) {
+        res = res + i + '.' + y + '\n';
+        if (y === 1) break qwer;
+    }
+
+}
+
+console.log(res);
+// #
+// ##
+// ###
+// ####
+// #####
+
+//     #
+//    ##
+//   ###
+//  ####
+// #####
+
+//     #   #
+//    ##   ##
+//   ###   ###
+//  ####   ####
+// #####   #####
+

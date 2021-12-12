8a1095f58bfbc912b18c282e5ba3344a691373ea
Dmytro Lieshchyn <resolushion@gmail.com>
2021-12-13 01:15:21 +0200

    80%

--- students/dima_lieshchyn/29_js_loops/src/js/script.js
+++ students/dima_lieshchyn/29_js_loops/src/js/script.js
@@ -72,31 +72,126 @@
 // }
 // console.log(summResult);
 //4.Запитай у користувача 2 числа і знайди найбільший спільний дільник.
-const userFNum = +prompt('Введіть перше число');
-const userSNum = +prompt('Введіть друге число');
-console.log(userFNum % userSNum);
+// const userFNum = +prompt('Введіть перше число');
+// const userSNum = +prompt('Введіть друге число');
+// let tempFirst = userFNum;
+// let tempSecond = userSNum;
+// let result = null;
+// while (tempFirst != 0 && tempSecond != 0) {
+//     if (tempFirst > tempSecond) {
+//         tempFirst = tempFirst % tempSecond;
+//         console.log(tempFirst);
+//     } else {
+//         tempSecond = tempSecond % tempFirst;
+//         console.log(tempSecond);
+//     }
+// }
+// result = tempFirst + tempSecond;
+// console.log(result);
 
 //5.Запитай у користувача число і виведи всі дільники цього числа.
+// const userNumber = +prompt('Введіть число');
+
+// for (let i = 0; i <= userNumber; i++) {
+//     if (userNumber % i == 0 && i !== 1) {
+//         console.log(i);
+//     }
+// }
 
 
 // --Норма--
 
 //1.Запитай у користувача п’ятирозрядне число і визначи, чи є воно паліндромом.
-
-
+// const fiveDigits = +prompt('Введіть п`ятизначне число');
+// let firstNum = null;
+// let secondNum = null;
+// let fourNum = null;
+// let fiveNum = null;
+// if (fiveDigits <= 100000 && fiveDigits > 10001) {
+//     firstNum = fiveDigits / 10000;
+//     secondNum = (fiveDigits / 1000) % 10;
+//     fourNum = (fiveDigits / 10) % 10;
+//     fiveNum = fiveDigits % 10;
+//     if (parseInt(firstNum) === parseInt(fiveNum) &&
+//         parseInt(secondNum) === parseInt(fourNum)) {
+//         console.log('Число паліндром');
+//     } else {
+//         console.log('Число не паліндром');
+//     }
+// } else {
+//     console.log('Число не п`ятизначне');
+// }
 //2.Запитай у користувача суму покупки і виведи суму до оплати зі знижкою:
 //1. від 200 до 300 - знижка буде 3%;
 //2. від 300 до 500 - 5%;
 //3. від 500 і вище - 7%.
+// const userPrice = +prompt('Введіть суму покупки');
+// let twoH = 200;
+// let threeH = 300;
+// let fiveH = 500;
+// let priceResult = null;
+// if (userPrice >= twoH) {
+//     if (userPrice >= twoH && userPrice < threeH + 1) {
+//         priceResult = userPrice - (userPrice * 3 / 100);
+//     } else if (userPrice >= threeH && userPrice < fiveH + 1) {
+//         priceResult = userPrice - (userPrice * 5 / 100);
+//     } else if (userPrice > fiveH) {
+//         priceResult = userPrice - (userPrice * 7 / 100);
+//     }
+// } else {
+//     console.log('Ваша сума менша за 200');
+// }
+// console.log(priceResult);
 
 //3.Запитай у користувача 10 чисел і порахуй, скільки він ввів додатніх,
 //від’ємних і нулів. При цьому також порахуй, скільки з них парних і непарних.
 //Виведи статистику на екран. Враховуй, що достатньо однієї змінної (не 10) для введення чисел користувачем.
-
+// const maxNumbers = 10;
+// let tenNumbers = '';
+// let positiv = 0;
+// let negativ = 0;
+// let countZero = 0;
+// let countPaired = 0;
+// let countUnpaired = 0;
+// for (let i = 0; i < maxNumbers; i++) {
+//     tenNumbers = prompt('Введіть ' + (i + 1) + ' число');
+//     if (tenNumbers > 0) {
+//         if (tenNumbers % 2 == 0) {
+//             countPaired++;
+//             positiv++;
+//         } else if (tenNumbers % 2 !== 0) {
+//             countUnpaired++;
+//             positiv++;
+//         }
+//     } else if (tenNumbers < 0) {
+//         if (tenNumbers % 2 == 0) {
+//             countPaired++;
+//             negativ++;
+//         } else if (tenNumbers % 2 !== 0) {
+//             countUnpaired++;
+//             negativ++;
+//         }
+//     } else {
+//         countZero++;
+//     }
+// }
+// console.log('Додатніх: ' + positiv + '\nВід`ємних: ' + negativ +
+//     '\nНулів: ' + countZero + '\nПарних: ' + countPaired + '\nНепарних: ' + countUnpaired);
 
 //4.Зацикли висновок днів тижня таким чином: «День тижня.
 //Хочеш побачити наступний день? » і так до тих пір, поки користувач натискає OK.
-
+// const dayOfWeek = ['Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П`ятниця', 'Субота', 'Неділя'];
+// let tempZero = 0;
+// let userDay = '';
+// while (true) {
+//     userDay = confirm(dayOfWeek[temp] + ". Хочеш побачити наступний день?");
+//     tempZero++;
+//     if (tempZero > 6) {
+//         tempZero = 0;
+//     } else if (!userDay) {
+//         break;
+//     }
+// }
 
 // --Максимум--
 
@@ -106,10 +201,54 @@ console.log(userFNum % userSNum);
 //Залежно від того що вказав користувач, зменшуй діапазон. Початковий діапазон від 0 до 100,
 //поділи навпіл і отримай 50. Якщо користувач вказав, що його число> 50, то зміни діапазон на від 51 до 100.
 //І так до тих пір, поки користувач не вибере == N.
+// alert('Загадай число від 0 до 100');
+// let temp = 100;
+// let res = '';
+// while (res) {
+//     temp = temp / 2;
+//     let userQuestion = confirm('Ваше число більше за ' + temp);
+//     if (userQuestion) {
+//         res = confirm('Ваше число ' + temp + ' ?');
+//         temp = temp + temp / 2;
+//         userQuestion = confirm('Ваше число більше за ' + temp);
+//         res = confirm('Ваше число ' + temp + ' ?');
+//         if (userQuestion) {
+
+//             userQuestion = confirm('Ваше число більше за ' + temp);
+//         }
+//     }
+// }
 
 
 //2.Виведи таблицю множення для всіх чисел від 2 до 9. Кожне число необхідно помножити на числа від 1 до 10.
-
+// for (let i = 2; i <= 9; i++) {
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(i + ' x ' + j + ' = ' + (i * j));
+//     }
+//     console.log('------------');
+// }
 
 //3.Запитай дату (день, місяць, рік) і виведи наступну за нею дату.
 //Враховуй можливість переходу на наступний місяць, рік, а також високосний рік.
+
+//Задание на вывод решеток
+const userNumFigure = +prompt('Введите число');
+let tempUse = userNumFigure;
+let temp = '';
+const userSymbol = '#';
+const userSpace = '-';
+
+// for (let i = 0; i < userNumFigure; i++) {
+//     temp = temp + userSymbol;
+//     console.log(temp);
+// }
+for (let i = 0; i < userNumFigure; i++) {
+    for (let j = tempUse; j > 0; j--) {
+        temp = temp + userSpace;
+    }
+
+
+    console.log(temp);
+    tempUse--;
+    temp = '';
+}

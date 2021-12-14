318bb4556d6bb20057b8c427ddc83b9d1500dd81
Dmytro Lieshchyn <resolushion@gmail.com>
2021-12-14 22:42:27 +0200

    fix errors

--- students/dima_lieshchyn/26_js_basics/js/script.js
+++ students/dima_lieshchyn/26_js_basics/js/script.js
@@ -8,16 +8,16 @@
 
 // Правильні
 //const userName;
-//const userSurname;
-//const FirstName;
-//const last_name;
+// const userSurname;
+// const FirstName;
+// const last_name;
 
 // Неправильні
 // const Name - в переменных использование некоторых слов может вызвать ошибку,
 //так как их заведомо использует браузер.
 
 // const First+name; - использование математических операторов запрещаеться.
-// const last-Name; 
+// const last-Name;
 
 // const SuRnAmE; - не рациональное использование Camel Case.
 //Необходимо логически использовать верхний регистр. 
@@ -26,7 +26,7 @@
 
 // const first Name; - пробелов в переменной не должно быть.
 
-// const 1name; - переменная не должна начинаться с цифер.
+// const 1name; - переменная не должна начинаться с цифр.
 
 /*---------------------------------------*/
 
@@ -56,6 +56,7 @@
 //Задание 1
 //Запитай ім’я користувача та виведи у відповідь “Привіт, *ім’я*”;
 const userName = prompt('Enter your name');
+const userResult = ('Hello, ' + userName);
 console.log('Hello, ' + userName);
 /*---------------------------------------*/
 //Задание 2
@@ -63,37 +64,37 @@ console.log('Hello, ' + userName);
 //і виведи результат. Поточний рік вкажи в коді як константу;
 const userYearOfBirht = +prompt('Enter year of your birth');
 const currentYear = 2021;
-let userAge = currentYear - userYearOfBirht;
-console.log('Your age is ' + userAge + ' years.');
+const userAge = 'Your age is ' + (currentYear - userYearOfBirht) + ' years.';
+console.log(userAge);
 /*---------------------------------------*/
 //Задание 3
 //Запитай у користувача довжину сторони квадрату і виведи периметр цього квадрата
 const userSquareLength = +prompt('Enter a length of your square');
 const sideCount = 4;
-let squarePerimetr = userSquareLength * sideCount;
-console.log('Perimetr of your sqare is ' + squarePerimetr);
+const squarePerimetr = 'Perimetr of your sqare is ' + (userSquareLength * sideCount);
+console.log(squarePerimetr);
 /*---------------------------------------*/
 //-----Maximum-----
 /*---------------------------------------*/
 //Задание 1
 //Запитай у користувача радіус кола і виведи площу такої окружності.
 const userRadius = +prompt('Enter your radius');
-let area = Math.PI * Math.pow(userRadius, 2);
-console.log('Your area is ' + area);
+const area = 'Your area is ' + (Math.PI * Math.pow(userRadius, 2));
+console.log(area);
 /*---------------------------------------*/
 //Задание 2
 //Запитай у користувача відстань в кілометрах між двома містами і за скільки
 //годин він хоче дістатися. Порахуй швидкість, з якою необхідно рухатися, щоб встигнути вчасно.
 const userDistance = +prompt('Enter your distance in kilometres');
 const userTime = +prompt('Enter how many hours you have');
-let currentSpeed = userDistance / userTime;
-console.log('Speed which you need is ' + currentSpeed + ' km/h');
+const currentSpeed = 'Speed which you need is ' + (userDistance / userTime) + ' km/h';
+console.log(currentSpeed);
 /*---------------------------------------*/
 //Задание 3
 //Реалізуй конвертор валют. Користувач вводить долари, програма переводить їх в євро.
 //Курс валют зберігається в константі.
 const euroLatency = 0.881;
 const userDollars = +prompt('Enter dollars amount');
-let userEuro = userDollars * euroLatency;
-console.log(userDollars + ' $ ' + ' = ' + userEuro + ' euro');
+const userEuroResult = userDollars + ' $ ' + ' = ' + (userDollars * euroLatency) + ' €';
+console.log(userEuroResult);
 /*---------------------------------------*/

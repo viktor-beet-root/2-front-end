ebb9fcb2bbce142bf924692d4b58a768fd58dbe7
Dmytro Lieshchyn <resolushion@gmail.com>
2021-12-17 00:29:34 +0200

    complete

--- students/dima_lieshchyn/32_js_functions/src/js/script.js
+++ students/dima_lieshchyn/32_js_functions/src/js/script.js
@@ -1,28 +1,30 @@
 (function () {
-    // --Мінімум--
-    //1.Напиши функцію, яка приймає час (години, хвилини, секунди)
-    // і виводить його на екран у форматі «гг: хв: сс» .
-    // Якщо при виконанні функції хвилини і / або секунди не були надані, то виводити їх як 00.
+    // ------------------------------------------------------------------------
+    //Help starter-pack
     const maxMS = 60;
     const maxH = 23;
     const secH = 3600;
     const zero = '0';
     const dots = ':';
+    function timeHelp(timeNum) {
+        if (timeNum < 10) return zero + timeNum;
+        return timeNum;
+    }
+    // ------------------------------------------------------------------------
+    // --Мінімум--
+    //1.Напиши функцію, яка приймає час (години, хвилини, секунди)
+    // і виводить його на екран у форматі «гг: хв: сс» .
+    // Якщо при виконанні функції хвилини і / або секунди не були надані, то виводити їх як 00.
 
     function timeFunc(hours = 0, minutes = 0, seconds = 0) {
         let timeResult = '';
         if (hours < 0 || minutes < 0 || seconds < 0) return 'Время не может быть отрицательным';
         else if (hours > maxH) return 'Часы не могут быль больше 23';
         else if (minutes > maxMS || seconds > maxMS) return 'Значение не может быть больше 60';
-
-        function timeHelp(timeNum) {
-            if (timeNum < 10) return zero + timeNum;
-            return timeNum;
-        }
         timeResult = timeHelp(hours) + dots + timeHelp(minutes) + dots + timeHelp(seconds);
         return timeResult;
     }
-    console.log(timeFunc(20));
+    console.log(timeFunc(20, 2, 2));
     // ------------------------------------------------------------------------
     // 2.Напиши функцію, яка приймає години, хвилини і секунди і повертає цей час в секундах.
     function timeSec(h = 0, min = 0, sec = 0) {
@@ -44,37 +46,58 @@
     // хвилини і секунди і повертає у вигляді рядка «гг: хв: сс».
     function timeSeconds(secnds) {
         let timeResult = '';
-        secnds = secnds;
+        secnds = +secnds;
         if (secnds < 0) return 'Время не может быть отрицательным';
         if (secnds % secH == 0) {
             return secnds / secH;
         } else {
-            timeResult = Math.floor(secnds / secH) + dots;
+            timeResult = timeHelp(Math.floor(secnds / secH)) + dots;
             secnds = secnds % secH;
             if (secnds % maxMS == 0) {
-                timeResult = timeResult + secnds / maxMS;
+                timeResult = timeResult + timeHelp(secnds / maxMS);
                 return timeResult;
             } else {
-                timeResult = Math.floor(secnds / secH) + dots;
-                secnds = secnds % secH;
-                timeResult = secnds;
-
+                timeResult = timeResult + timeHelp(Math.floor(secnds / maxMS)) + dots;
+                secnds = secnds % maxMS;
+                timeResult = timeResult + timeHelp(secnds);
+                return timeResult;
             }
         }
-
     }
-    console.log(timeSeconds(3662))
+    console.log(timeSeconds(7820))
     // ------------------------------------------------------------------------
     // --Норма--
     // 1.Напиши функцію, яка рахує різницю між датами.Функція приймає 6 параметрів,
     //  які описують 2 дати, і повертає результат у вигляді рядка «гг: хв: сс».
     //  При виконанні завдання використовуй функції з попередніх 2 - х завдань:
     //  спочатку обидві дати переведи в секунди, дізнайся різницю в секундах, а потім різницю переведи назад в «гг: хв: сс».
-
+    function differentTime(h1 = 0, m1 = 0, s1 = 0, h2 = 0, m2 = 0, s2 = 0) {
+        const firstT = timeSec(h1, m1, s1);
+        const secondT = timeSec(h2, m2, s2);
+        let differentResult = '';
+        if (firstT > secondT) {
+            differentResult = timeSeconds(firstT - secondT);
+        } else {
+            differentResult = timeSeconds(secondT - firstT);
+        }
+        return differentResult;
+    }
+    console.log(differentTime(6, 32, 4, 2, 4, 5));
     // ------------------------------------------------------------------------
     // --Максимум-- 
     // Напиши функцію, яка приймає час(години, хвилини, секунди), повертає функцію,
     // яка замкнута на ці змінні і керує ними.Якщо функції передати, наприклад, 3660 секунд, час має змінитися на 1 г.та 1 хв.
-
+    function timeManager(hr, mn, sc) {
+        let currentTime = timeSec(hr, mn, sc);
+        return function (secs = 0) {
+            currentTime = currentTime + secs;
+            return timeSeconds(currentTime);
+        }
+    }
+    let addTime = timeManager(2, 3, 4);
+    addTime(1)
+    addTime(3600)
+    addTime(3601)
+    console.log(addTime());
     // ------------------------------------------------------------------------
 })();

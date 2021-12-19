cbce84f00d806a43c0a401543a981cd8509833ab
Dmytro Lieshchyn <resolushion@gmail.com>
2021-12-19 22:13:55 +0200

    complete 45%

--- students/dima_lieshchyn/33_js_objects/build/js/script.js
+++ students/dima_lieshchyn/33_js_objects/build/js/script.js
@@ -5,39 +5,51 @@
     manufacturer: 'BMW',
     model: 'X5',
     yearOfProd: 2020,
-    speedAverage: 50,
-    tankVolume: 45,
-    fuelAverage100: 15,
+    speedAverage: 100,
+    tankVolume: 100,
+    fuelAverage100: 10,
     drivers: {
       names: 'Andrey',
-      licence: 'Andrey 0'
+      licence: 'Andrey 1'
     },
     carInfo: function carInfo() {
-      console.log("Manufacturer:".concat(this.manufacturer, "\nModel: ").concat(this.model, "\nYear of produce: ").concat(this.yearOfProd, "\nAverage speed: ").concat(this.speedAverage, " km/h\nVolume of the tank: ").concat(this.tankVolume, " litres\nFuel average for 100 km: ").concat(this.fuelAverage100, " km/h\nDrivers: ").concat(this.drivers.names));
+      console.log("Manufacturer:".concat(this.manufacturer, "\nModel: ").concat(this.model, "\nYear of produce: ").concat(this.yearOfProd, "\nAverage speed: ").concat(this.speedAverage, " km/h\nVolume of the tank: ").concat(this.tankVolume, " litres\nFuel average for 100 km: ").concat(this.fuelAverage100, " km/h\n%cDrivers: %c").concat(this.drivers.names), 'font-weight:bold; color:red;font-size:14px;', 'color:white');
     },
+    //функция вывода информации по водителю по имени
     showDriver: function showDriver(driverName) {
+      //воспомагательные переменные
       var dName = 0;
       var lName = 0;
       var nameRes = '';
-      var licenceRes = 0;
+      var licenceRes = 0; //проверка на правильность ввода имени водителя
 
       if (typeof driverName === 'string' && driverName !== '') {
-        dName = this.drivers.names.indexOf(driverName);
-        nameRes = 'Driver: ' + this.drivers.names.slice(dName, dName + driverName.length);
-        console.log(nameRes);
-        lName = this.drivers.licence.indexOf(driverName);
-        licenceRes = this.drivers.licence.slice(lName + driverName.length + 1, lName + driverName.length + 2);
-        console.log('Licence: ' + licenceRes);
+        //запись индекса начала имени в строке имен
+        dName = this.drivers.names.indexOf(driverName); //итоговая запись имени путём вырезания из строки
+
+        nameRes = 'Driver: ' + this.drivers.names.slice(dName, dName + driverName.length); //запись индекса начала лицензии прав в строке licence
+
+        lName = this.drivers.licence.indexOf(driverName); //итоговая запись лицензии на вождение путём вырезания из строки
+
+        licenceRes = 'Licence: ' + this.drivers.licence.slice(lName + driverName.length + 1, lName + driverName.length + 2); //вывод информации
+
+        console.log(nameRes + '\n' + licenceRes); //возврат для дальнешего использования
+
         return licenceRes;
       }
     },
-    addDriver: function addDriver(driverName, driverLicence) {
+    addDriver: function addDriver(driverName) {
+      var driverLicence = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+
+      //проверка на правильность ввода имени водителя
       if (typeof driverName === 'string' && driverName !== '') {
-        this.drivers.names = this.drivers.names + ', ' + driverName;
+        //конкатенация строки, добавление водителя
+        this.drivers.names = this.drivers.names + ', ' + driverName; //проверка на правильность ввода прав для водителя
 
         if (typeof driverLicence === 'boolean') {
+          //конкатенция если есть права
           if (driverLicence) {
-            this.drivers.licence = this.drivers.licence + ',' + driverName + ' ' + '1';
+            this.drivers.licence = this.drivers.licence + ',' + driverName + ' ' + '1'; //конкатенция если нет прав
           } else {
             this.drivers.licence = this.drivers.licence + ',' + driverName + ' ' + '0';
           }
@@ -49,17 +61,76 @@
         this.tankVolume = this.tankVolume + fuel;
       }
     },
-    calculateTrip: function calculateTrip(driverName) {
+    calculateTrip: function calculateTrip(distance, driverName) {
+      //переменная для записи результата
+      var hoursTrip = 0; //константа значения максимальной безпрерывной поездки
+
+      var maxTrip = 4; //константа максимальной дистанции на полном баке
+
+      var maxTripKm = this.tankVolume / this.fuelAverage100 * 100; //проверка на правильность ввода имени водителя
+
       if (typeof driverName === 'string' && driverName !== '') {
-        if (this.showDriver()) {} else {
+        //если у водителя права
+        if (this.showDriver(driverName)) {
+          //проверка на правильность ввода дистанции
+          if (typeof distance === 'number' && distance > 0) {
+            //если дистанция меньше чем дистанция максимальной поездки на полном баке
+            if (distance < maxTripKm) {
+              //просчет колличества часов на поездку
+              hoursTrip = distance / this.speedAverage; //проверка и просчет сколько нужно пауз на каждые 4 часа
+
+              if (hoursTrip > maxTrip) {
+                //цикл на проверку для каждых четырех часов
+                for (var i = 1; i < hoursTrip; i++) {
+                  if (i % 4 == 0) {
+                    ++hoursTrip;
+                  }
+                }
+
+                console.log('Need ' + hoursTrip + ' hours');
+                return hoursTrip;
+              } else {
+                //условие если поездка меньше 4 часов
+                console.log('Need ' + hoursTrip + ' hours');
+                return hoursTrip;
+              }
+            } else {
+              //условие если нехватит топлива на поездку
+              console.log('Need refueling a car');
+            }
+          } else {
+            //неправильный ввод дистанции
+            console.log('Need distance');
+          }
+        } else {
+          //у водителя нет прав
           console.log('Need licence');
         }
       }
     }
   };
   car.carInfo();
-  car.addDriver('Kolya', false);
-  car.showDriver('Kolya');
-  car.carInfo();
-  car.calculateTrip('Kolya');
+  car.calculateTrip(800, 'Andrey');
+  var timeHelper = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    showTime: function showTime() {
+      console.log(this.timeConvert(this.hours) + ':' + this.timeConvert(this.minutes) + ':' + this.timeConvert(this.seconds));
+    },
+    timeConvert: function timeConvert(timeCheckItem) {
+      if (timeCheckItem >= 0 && timeCheckItem < 10) {
+        return '0' + timeCheckItem;
+      }
+    },
+    timeAddBySeconds: function timeAddBySeconds(secondsChange) {
+      if (secondsChange % 3600 == 0) {
+        this.hours = this.hours + Math.floor(secondsChange / 3600);
+      } else {
+        this.hours = this.hours + Math.floor(secondsChange / 3600);
+      }
+    }
+  };
+  timeHelper.timeAddBySeconds(7200);
+  timeHelper.showTime();
 })();
--- students/dima_lieshchyn/33_js_objects/src/js/script.js
+++ students/dima_lieshchyn/33_js_objects/src/js/script.js
@@ -3,68 +3,140 @@
         manufacturer: 'BMW',
         model: 'X5',
         yearOfProd: 2020,
-        speedAverage: 50,
-        tankVolume: 45,
-        fuelAverage100: 15,
+        speedAverage: 100,
+        tankVolume: 100,
+        fuelAverage100: 10,
         drivers: {
             names: 'Andrey',
-            licence: 'Andrey 0',
+            licence: 'Andrey 1',
         },
-        carInfo: function () {
+        carInfo() {
             console.log(`Manufacturer:${this.manufacturer}
 Model: ${this.model}
 Year of produce: ${this.yearOfProd}
 Average speed: ${this.speedAverage} km/h
 Volume of the tank: ${this.tankVolume} litres
 Fuel average for 100 km: ${this.fuelAverage100} km/h
-Drivers: ${this.drivers.names}`)
+%cDrivers: %c${this.drivers.names}`, 'font-weight:bold; color:red;font-size:14px;', 'color:white')
         },
-        showDriver: function (driverName) {
+        //функция вывода информации по водителю по имени
+        showDriver(driverName) {
+            //воспомагательные переменные
             let dName = 0;
             let lName = 0;
             let nameRes = '';
             let licenceRes = 0;
+            //проверка на правильность ввода имени водителя
             if (typeof driverName === 'string' && driverName !== '') {
+                //запись индекса начала имени в строке имен
                 dName = this.drivers.names.indexOf(driverName);
+                //итоговая запись имени путём вырезания из строки
                 nameRes = 'Driver: ' + this.drivers.names.slice(dName, dName + driverName.length);
-                console.log(nameRes);
+                //запись индекса начала лицензии прав в строке licence
                 lName = this.drivers.licence.indexOf(driverName);
-                licenceRes = this.drivers.licence.slice(lName + driverName.length + 1, lName + driverName.length + 2);
-                console.log('Licence: ' + licenceRes);
+                //итоговая запись лицензии на вождение путём вырезания из строки
+                licenceRes = 'Licence: ' + this.drivers.licence.slice(lName + driverName.length + 1, lName + driverName.length + 2);
+                //вывод информации
+                console.log(nameRes + '\n' + licenceRes);
+                //возврат для дальнешего использования
                 return licenceRes;
             }
         },
-        addDriver: function (driverName, driverLicence) {
+        addDriver(driverName, driverLicence = false) {
+            //проверка на правильность ввода имени водителя
             if (typeof driverName === 'string' && driverName !== '') {
+                //конкатенация строки, добавление водителя
                 this.drivers.names = this.drivers.names + ', ' + driverName;
+                //проверка на правильность ввода прав для водителя
                 if (typeof driverLicence === 'boolean') {
+                    //конкатенция если есть права
                     if (driverLicence) {
                         this.drivers.licence = this.drivers.licence + ',' + driverName + ' ' + '1';
+                        //конкатенция если нет прав
                     } else {
                         this.drivers.licence = this.drivers.licence + ',' + driverName + ' ' + '0';
                     }
                 }
             }
         },
-        refueling: function (fuel) {
+        refueling(fuel) {
             if (typeof fuel === 'number' && fuel > 0) {
                 this.tankVolume = this.tankVolume + fuel;
             }
         },
-        calculateTrip: function (driverName) {
+        calculateTrip(distance, driverName) {
+            //переменная для записи результата
+            let hoursTrip = 0;
+            //константа значения максимальной безпрерывной поездки
+            const maxTrip = 4;
+            //константа максимальной дистанции на полном баке
+            const maxTripKm = (this.tankVolume / this.fuelAverage100) * 100;
+            //проверка на правильность ввода имени водителя
             if (typeof driverName === 'string' && driverName !== '') {
-                if (this.showDriver()) {
-
+                //если у водителя права
+                if (this.showDriver(driverName)) {
+                    //проверка на правильность ввода дистанции
+                    if (typeof distance === 'number' && distance > 0) {
+                        //если дистанция меньше чем дистанция максимальной поездки на полном баке
+                        if (distance < maxTripKm) {
+                            //просчет колличества часов на поездку
+                            hoursTrip = distance / this.speedAverage;
+                            //проверка и просчет сколько нужно пауз на каждые 4 часа
+                            if (hoursTrip > maxTrip) {
+                                //цикл на проверку для каждых четырех часов
+                                for (let i = 1; i < hoursTrip; i++) {
+                                    if (i % 4 == 0) {
+                                        ++hoursTrip;
+                                    }
+                                }
+                                console.log(('Need ' + hoursTrip + ' hours'));
+                                return hoursTrip;
+                            } else {
+                                //условие если поездка меньше 4 часов
+                                console.log(('Need ' + hoursTrip + ' hours'));
+                                return hoursTrip;
+                            }
+                        } else {
+                            //условие если нехватит топлива на поездку
+                            console.log('Need refueling a car');
+                        }
+                    } else {
+                        //неправильный ввод дистанции
+                        console.log('Need distance');
+                    }
                 } else {
-                    console.log('Need licence')
+                    //у водителя нет прав
+                    console.log('Need licence');
                 }
             }
         },
     }
 
     car.carInfo();
-    car.addDriver('Kolya', false)
-    car.showDriver('Kolya')
-    car.carInfo();
-    car.calculateTrip('Kolya')
+    car.calculateTrip(800, 'Andrey');
+
+    // const timeHelper = {
+    //     hours: 0,
+    //     minutes: 0,
+    //     seconds: 0,
+    //     showTime() {
+    //         console.log(this.timeConvert(this.hours) + ':' + this.timeConvert(this.minutes) + ':' + this.timeConvert(this.seconds));
+    //     },
+    //     timeConvert(timeCheckItem) {
+    //         if (timeCheckItem >= 0 && timeCheckItem < 10) {
+    //             return '0' + timeCheckItem;
+    //         }
+    //     },
+    //     timeAddBySeconds(secondsChange) {
+    //         if (secondsChange % 3600 == 0) {
+    //             this.hours = this.hours + Math.floor(secondsChange / 3600);
+    //         } else {
+    //             this.hours = this.hours + Math.floor(secondsChange / 3600);
+
+    //         }
+    //     }
+
+    // }
+    // timeHelper.timeAddBySeconds(7200)
+    // timeHelper.showTime()
 })();

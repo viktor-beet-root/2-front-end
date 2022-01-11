6fa9bba202429f62e88afef6e7b1cf079aced52f
Dmytro Lieshchyn <resolushion@gmail.com>
2022-01-11 23:28:50 +0200

    complete

--- students/dima_lieshchyn/38_classes_1/src/js/Course.js
+++ students/dima_lieshchyn/38_classes_1/src/js/Course.js
@@ -32,4 +32,65 @@ Course.prototype.getStudentIndexById = function (idStudent) {
     return this.studentsList.map(student => student.id).indexOf(idStudent);
 };
 
+Course.prototype.getStudentById = function (idStudent) {
+    return this.studentsList.find(student => student.id === idStudent);
+};
+
+//1. Заменяет студента на другого (id,{student}).
+Course.prototype.changeStudentById = function (idStudent, student) {
+    const indexStudent = this.getStudentIndexById(idStudent);
+
+    if (!~indexStudent) return {};
+
+    return this.studentsList.splice(indexStudent, 1, student);
+};
+
+//2. Возвращает масив всех скилов студентов skillList => fullSkillList.
+
+Course.prototype.getFullSkillList = function () {
+    let fullSkillList = '';
+    this.studentsList.map(
+        student => fullSkillList = fullSkillList + student.skillList + ','
+    );
+    return fullSkillList;
+};
+
+
+//3. Возвращает масив всех скилов студентов skillList уникальный uniqSkillList.
+Course.prototype.getUniqSkillList = function () {
+    return this.getFullSkillList()
+        .split(',')
+        .filter((skill, index, arr) => arr.indexOf(skill) === index)
+        .join(', ')
+        .slice(0, -2);
+}
+
+//4. Сортировать студентов в зависимости 
+//от их скила(кол-во скилов length) принимает true false.
+Course.prototype.sortStudentsBySkill = function (switcher) {
+    switcher ?
+        this.studentsList.sort(
+            (a, b) => a.skillList.length - b.skillList.length
+        )
+        :
+        this.studentsList.sort(
+            (a, b) => b.skillList.length - a.skillList.length
+        );
+};
+
+
+//5. Метод возвращает студента по возрасту самого старого.
+Course.prototype.getOldestStudent = function () {
+    const temp = this.studentsList.slice();
+    return temp.sort((a, b) => b.age - a.age)[0];
+};
+
+
+//6. Метод молодого.
+Course.prototype.getYoungerStudent = function () {
+    const temp = this.studentsList.slice();
+    return temp.sort((a, b) => a.age - b.age)[0];
+};
+
 export default Course;
+
--- students/dima_lieshchyn/38_classes_1/src/js/script.js
+++ students/dima_lieshchyn/38_classes_1/src/js/script.js
@@ -14,28 +14,28 @@ frontend.addStudent({
     skillList: ['html', 'js', 'git']
 });
 frontend.addStudent({
-    name: 'Kolya',
-    email: 'kolya@gmail.com',
-    age: 24,
-    skillList: ['html', 'js']
-});
-frontend.addStudent({
     name: 'Denn',
     email: 'denn@gmail.com',
     age: 45,
     skillList: ['html', 'js', 'nodeJs', 'git', 'scss', 'css']
 });
+frontend.addStudent({
+    name: 'Kolya',
+    email: 'kolya@gmail.com',
+    age: 24,
+    skillList: ['html', 'js']
+});
 
-console.log(frontend.removeStudent(6));
 
+console.log(frontend.changeStudentById(1, {
+    name: 'Andrey',
+    email: 'andrey@gmail.com',
+    age: 12,
+    skillList: ['windows family']
+}));
+// console.log(frontend.getUniqSkillList());
+console.log(frontend.sortStudentsBySkill(0))
 console.log(frontend.getStudentsList());
-// console.log(makeStudentsScope(students));
+console.log(frontend.getOldestStudent());
+console.log(frontend.getYoungerStudent())
 
-
-//1. Заменяет студента на другого (id,{student}).
-//2. Возвращает масив всех скилов студентов skillList => fullSkillList.
-//3. Возвращает масив всех скилов студентов skillList уникальный uniqSkillList.
-//4. Сортировать студентов в зависимости от их скила(кол-во скилов length) принимает true false.
-//5. Метод возвращает студента по возрасту самого старого.
-//6. Метод молодого.
-//7. Диапазон принимает возвраста и возвращает студентов масив.

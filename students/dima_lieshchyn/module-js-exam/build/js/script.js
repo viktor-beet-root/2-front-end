8c7d5031c4892af169e24eddcbeab9187525114d
Dmytro Lieshchyn <resolushion@gmail.com>
2022-04-11 15:33:47 +0300

    50%

--- students/dima_lieshchyn/module-js-exam/build/js/script.js
+++ students/dima_lieshchyn/module-js-exam/build/js/script.js
@@ -1,8 +1,21 @@
 /******/ (function() { // webpackBootstrap
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+!function() {
 /*!**************************!*\
   !*** ./src/js/script.js ***!
   \**************************/
-
+$('.carousel').carousel({
+  interval: 3000
+});
+}();
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+!function() {
+/*!**************************!*\
+  !*** ./src/js/slider.js ***!
+  \**************************/
+// export default $;
+}();
 /******/ })()
 ;
+//# sourceMappingURL=script.js.map

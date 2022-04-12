cba51809928e15705259fc95f8a3bc9d45c73b2a
Dmytro Lieshchyn <resolushion@gmail.com>
2022-04-12 17:23:12 +0300

    html,css+

--- students/dima_lieshchyn/module-js-exam/src/js/script.js
+++ students/dima_lieshchyn/module-js-exam/src/js/script.js
@@ -1,3 +1,17 @@
+import $ from 'jquery';
 $('.carousel').carousel({
     interval: 3000,
 });
+
+$(function () {
+    $(".arrow").click(function () {
+        $("html:not(:animated),body:not(:animated)")
+            .animate({
+                scrollTop: $($(this).attr("href")).offset().top
+            }, 1000);
+        return false;
+    });
+});
+// const nav = document.querySelector('nav');
+// const arrow = document.querySelector('.arrow');
+// scrollTo(arrow, nav, 5000);

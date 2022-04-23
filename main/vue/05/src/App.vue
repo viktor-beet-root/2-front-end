969f2f87158ed747f8c8ced14c7c4aa50d1d8c22
Viktor Dehtiarov <4059858@gmail.com>
2022-04-23 13:20:17 +0300

    Lesson 60. Vue - 5 Vue CLI. Componets

---
+++ main/vue/05/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <beetroot-test :test="test" :list="list" @setTest="setTest">
+        <h3>for slot {{ test }}</h3>
+    </beetroot-test>
+</template>
+
+<script>
+import BeetrootTest from "./components/BeetrootTest.vue";
+
+export default {
+    name: "App",
+    components: { BeetrootTest },
+    data() {
+        return {
+            test: "sdfd",
+            firstName: "",
+            lastName: "",
+            list: ["sdf", "asdf"],
+        };
+    },
+    methods: {
+        setTest(value) {
+            this.test = value;
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ main/vue/05/src/components/BeetrootTest.vue
@@ -0,0 +1,37 @@
+<template>
+    <slot> <p>Not data</p> </slot>
+    <h2>{{ test }}</h2>
+    <p v-for="item in list" :key="item">{{ item }}</p>
+    <button @click="setTest">tsdf</button>
+</template>
+
+<script>
+export default {
+    name: "beetroot-test",
+    emits: ["setTest"],
+    props: {
+        test: String,
+        list: {
+            type: Array,
+            required: true,
+            default() {
+                return [];
+            },
+            validator(value) {
+                return value.length;
+            },
+        },
+    },
+    data() {
+        return {};
+    },
+    methods: {
+        setTest() {
+            const val = prompt("set value test");
+            this.$emit("setTest", val);
+        },
+    },
+};
+</script>
+
+<style lang="scss"></style>

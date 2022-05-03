7513f44e996120946df683145681d944b1f1b408
Dmytro Lieshchyn <resolushion@gmail.com>
2022-05-03 18:35:40 +0300

    create vue project

---
+++ students/dima_lieshchyn/final-exam-vue/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  rules: {
+    "prettier/prettier": [
+      "warn",
+      { "endOfLine": "auto", "tabWidth": 4 }
+    ],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
